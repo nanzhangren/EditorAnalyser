11ca520a6606c22be49ba96f2e9212b72893ff32
nanzhangren <trustfei@gmail.com>
2018-09-17 23:53:20 +0800

    Add webpack.

---
+++ src/main.js
@@ -0,0 +1,17 @@
+/*
+ * @description Entry file.
+ * @author Zero Zhang
+ */
+
+
+/*
+ * Analyse the given content.
+ * @param {string} content The analysed content.
+ * @return {string} The output content string.
+ */
+function analyse(content) {
+    if (!content) {
+        return '';
+    }
+    //
+}
